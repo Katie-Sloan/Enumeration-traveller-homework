12d74b1670f989af90da949d36bd33919ff367f8
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-18 20:12:27 +0000

    completed extensions

--- models/traveller.js
+++ models/traveller.js
@@ -11,27 +11,34 @@ Traveller.prototype.getJourneyStartLocations = function() {
 Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
     return journey.endLocation;
-  })
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
   return this.journeys.filter((journey) => {
     return journey.transport === transport;
-  })
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   return this.journeys.filter((journey) => {
     return journey.distance >= minDistance;
-  })
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const totalDistance = this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance += journey.distance; 
+  },0);
+  return totalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const transportList = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  const set = new Set(transportList);
+  return Array.from(set);
 };
 
 
